639b9bc77bb592b65ad1bace497c941bfa885e35
Rodrigo Nogueira <rdg6design@gmail.com>
2020-04-24 20:54:37 -0300

    search filter

--- routes/filter.js
+++ routes/filter.js
@@ -4,8 +4,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    // const filtro = req.query ? req.query.f : ''
-    axios.get("http://agenciabluefoot.vtexcommercestable.com.br/buscaautocomplete/?productNameContains=")
+    const filtro = req.query ? req.query.f : ''
+    axios.get("http://agenciabluefoot.vtexcommercestable.com.br/buscaautocomplete/?productNameContains="+filtro)
         .then((result)=>{
             console.log(result.data)
             res.json(result.data)
